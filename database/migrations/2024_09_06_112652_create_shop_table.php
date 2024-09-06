61e5112b45a83fe8e4de1671d1a18cff8cadaa39
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-06 19:25:28 +0700

    schema shop

---
+++ database/migrations/2024_09_06_112652_create_shop_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shop', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('desc');
+            $table->double('lat')->nullable();
+            $table->double('lng')->nullable();
+            $table->string('address')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shop');
+    }
+};

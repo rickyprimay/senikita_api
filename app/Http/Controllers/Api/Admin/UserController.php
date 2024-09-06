8857f773578574c6a3fee1442eec9ad2c3e77511
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-06 17:04:55 +0700

    adding method get all user

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getAllUsers()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'message' => 'All users retrieved successfully',
+            'data' => $users
+        ], 200);
+    }
 }

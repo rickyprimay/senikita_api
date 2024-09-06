fe913158f5f6bc2453720e35fe4998dbd2173222
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-06 17:21:34 +0700

    make group route auth

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,17 @@ use App\Http\Controllers\Api\Admin\UserController;
 use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::post('verify-otp', [AuthController::class, 'verifyOTP']);
-Route::post('resend-otp', [AuthController::class, 'resendOTP']);
-Route::middleware('auth:api')->post('logout', [AuthController::class, 'logout']);
+
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('verify-otp', [AuthController::class, 'verifyOTP']);
+    Route::post('resend-otp', [AuthController::class, 'resendOTP']);
+    Route::middleware('auth:api')->post('logout', [AuthController::class, 'logout']);
+});
 
 Route::get('users', [UserController::class, 'getAllUsers']);
+Route::get('users/{id}', [UserController::class, 'getUser']);
+Route::post('users', [UserController::class, 'createUser']);
+Route::put('users/{id}', [UserController::class, 'updateUser']); 
+Route::delete('users/{id}', [UserController::class, 'deleteUser']);

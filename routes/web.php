05fe09a57d1d73b3b8a6465cfa8c2079bb4b04d0
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-07 14:27:05 +0700

    make response abort

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hidden-login', function (){
+    return response()->json([
+        'message' => 'Unauthorized',
+        'code' => 401
+    ], 401);
+})->name('login');

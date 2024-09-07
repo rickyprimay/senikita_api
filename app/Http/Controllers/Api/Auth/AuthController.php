098c6c3d3402cc3639498d43a2142b2320e7bcfb
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-07 14:17:31 +0700

    modify payload auth

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -24,13 +24,11 @@ class AuthController extends Controller
         ]);
 
         if (User::where('email', $request->email)->exists()) {
-            return response()->json(
-                [
-                    'message' => 'Email already registered',
-                    'code' => 409,
-                ],
-                409,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Email already registered',
+                'code' => 409,
+            ], 409);
         }
 
         $otp = User::generateOTP();
@@ -50,14 +48,12 @@ class AuthController extends Controller
 
         Mail::to($credentials->email)->send(new VerificationCodeMail($details));
 
-        return response()->json(
-            [
-                'message' => 'Register Success, check your email to verification OTP',
-                'code' => 201,
-                'user' => $credentials,
-            ],
-            201,
-        );
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Register Success, check your email to verify OTP',
+            'code' => 201,
+            'user' => $credentials,
+        ], 201);
     }
 
     public function verifyOTP(Request $request)
@@ -70,35 +66,29 @@ class AuthController extends Controller
         $credential = User::where('email', $request->email)->first();
 
         if (!$credential) {
-            return response()->json(
-                [
-                    'message' => 'User not found',
-                    'code' => 404,
-                ],
-                404,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found',
+                'code' => 404,
+            ], 404);
         }
 
         if ($credential->otp !== $request->otp) {
-            return response()->json(
-                [
-                    'message' => 'Invalid OTP',
-                    'code' => 400,
-                ],
-                400,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid OTP',
+                'code' => 400,
+            ], 400);
         }
 
         $otpExpiryMinutes = 5;
         $otpSentAt = Carbon::parse($credential->otp_sent_at);
         if ($otpSentAt->addMinutes($otpExpiryMinutes)->isPast()) {
-            return response()->json(
-                [
-                    'message' => 'OTP has expired',
-                    'code' => 400,
-                ],
-                400,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => 'OTP has expired',
+                'code' => 400,
+            ], 400);
         }
 
         $token = JWTAuth::fromUser($credential);
@@ -108,18 +98,17 @@ class AuthController extends Controller
         $credential->email_verified_at = Carbon::now();
         $credential->save();
 
-        return response()->json(
-            [
-                'message' => 'OTP verified successfully',
-                'code' => 200,
-                'user' => $credential,
-                'access_token' => $token,
-                'token_type' => 'Bearer',
-                'expires_in' => config('jwt.ttl') * 60,
-            ],
-            200,
-        );
+        return response()->json([
+            'status' => 'success',
+            'message' => 'OTP verified successfully',
+            'code' => 200,
+            'user' => $credential,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => config('jwt.ttl') * 60,
+        ], 200);
     }
+
     public function resendOTP(Request $request)
     {
         $validatedData = $request->validate([
@@ -129,36 +118,30 @@ class AuthController extends Controller
         $credentials = User::where('email', $request->email)->first();
 
         if (!$credentials) {
-            return response()->json(
-                [
-                    'message' => 'User not found',
-                    'code' => 404,
-                ],
-                404,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found',
+                'code' => 404,
+            ], 404);
         }
 
         if ($credentials->email_verified_at) {
-            return response()->json(
-                [
-                    'message' => 'Email is already verified',
-                    'code' => 400,
-                ],
-                400,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Email is already verified',
+                'code' => 400,
+            ], 400);
         }
 
         $resendInterval = 1;
         if ($credentials->otp_sent_at && Carbon::parse($credentials->otp_sent_at)->diffInMinutes(Carbon::now()) < $resendInterval) {
             $remainingTime = 60 - Carbon::parse($credentials->otp_sent_at)->diffInSeconds(Carbon::now());
-            return response()->json(
-                [
-                    'message' => "You can request a new OTP after {$remainingTime} seconds",
-                    'remaining_times' => $remainingTime,
-                    'code' => 429,
-                ],
-                429,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => "You can request a new OTP after {$remainingTime} seconds",
+                'remaining_times' => $remainingTime,
+                'code' => 429,
+            ], 429);
         }
 
         $otp = User::generateOTP();
@@ -174,13 +157,11 @@ class AuthController extends Controller
 
         Mail::to($credentials->email)->send(new VerificationCodeMail($details));
 
-        return response()->json(
-            [
-                'message' => 'OTP has been resent, please check your email',
-                'code' => 200,
-            ],
-            200,
-        );
+        return response()->json([
+            'status' => 'success',
+            'message' => 'OTP has been resent, please check your email',
+            'code' => 200,
+        ], 200);
     }
 
     public function login(Request $request)
@@ -188,7 +169,11 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (!($token = JWTAuth::attempt($credentials))) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+                'code' => 401,
+            ], 401);
         }
 
         $credentials = User::where('email', $request->email)->first();
@@ -196,7 +181,7 @@ class AuthController extends Controller
         if (is_null($credentials->email_verified_at)) {
             $otp = User::generateOTP();
 
-            $credentials->otp = User::generateOTP();
+            $credentials->otp = $otp;
             $credentials->otp_sent_at = now();
             $credentials->save();
 
@@ -206,21 +191,20 @@ class AuthController extends Controller
             ];
             Mail::to($credentials->email)->send(new VerificationCodeMail($details));
 
-            return response()->json(
-                [
-                    'error' => 'Email not verified. Please check your email to verify your OTP.',
-                    'code' => 403,
-                ],
-                403,
-            );
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Email not verified. Please check your email to verify your OTP.',
+                'code' => 403,
+            ], 403);
         }
 
         $shop = Shop::where('user_id', $credentials->id)->first();
 
         if ($shop) {
             return response()->json([
-                'code' => 200,
+                'status' => 'success',
                 'message' => 'Login successful',
+                'code' => 200,
                 'token' => $token,
                 'shop' => $shop,
             ], 200);
@@ -228,39 +212,55 @@ class AuthController extends Controller
 
         return $this->respondWithToken($token);
     }
+
     protected function respondWithToken($token)
     {
         return response()->json([
+            'status' => 'success',
             'access_token' => $token,
             'token_type' => 'Bearer',
             'expires_in' => config('jwt.ttl') * 60,
-        ]);
+            'code' => 200,
+        ], 200);
     }
+
     public function logout()
     {
         try {
             JWTAuth::invalidate(JWTAuth::getToken());
 
             return response()->json([
+                'status' => 'success',
                 'message' => 'Successfully logged out',
-            ]);
+                'code' => 200,
+            ], 200);
         } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-            return response()->json(['error' => 'Token is invalid'], 401);
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Token is invalid',
+                'code' => 401,
+            ], 401);
         }
     }
+
     public function refreshToken()
     {
         try {
-            // Menggunakan JWTAuth untuk melakukan refresh token
             $newToken = JWTAuth::refresh(JWTAuth::getToken());
 
             return response()->json([
+                'status' => 'success',
                 'access_token' => $newToken,
                 'token_type' => 'Bearer',
                 'expires_in' => config('jwt.ttl') * 60,
-            ]);
+                'code' => 200,
+            ], 200);
         } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-            return response()->json(['error' => 'Token is invalid'], 401);
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Token is invalid',
+                'code' => 401,
+            ], 401);
         }
     }
 }

9df531ff12ca8c37193ad699fde5010cbbb2a756
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-17 10:45:24 +0700

    add api get city by province

--- app/Http/Controllers/Api/City/CityController.php
+++ app/Http/Controllers/Api/City/CityController.php
@@ -5,12 +5,41 @@ namespace App\Http\Controllers\Api\City;
 use App\Http\Controllers\Controller;
 use App\Models\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CityController extends Controller
 {
     public function index()
     {
         $cities = City::all();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Cities retrieved successfully',
+            'cities' => $cities,
+            'code' => 200,
+        ], 200);
+    }
+
+    public function getCitiesByProvince(Request $request)
+    {
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'province_id' => 'required|integer|exists:provinces,id', // Pastikan 'provinces' adalah nama tabel provinsi Anda
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $provinceId = $request->input('province_id');
+
+        $cities = City::where('province_id', $provinceId)->get();
 
         return response()->json([
             'status' => 'success',
